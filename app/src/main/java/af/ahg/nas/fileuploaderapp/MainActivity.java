30247a41d898bce56920c4d792af6623eda53ba9
nasrullahmohseni <nasrullah.mohseni@ahg.af>
2018-09-13 15:34:46 +0430

    Initialy Commits

---
+++ app/src/main/java/af/ahg/nas/fileuploaderapp/MainActivity.java
@@ -0,0 +1,132 @@
+package af.ahg.nas.fileuploaderapp;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.support.v4.content.CursorLoader;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.error.VolleyError;
+import com.android.volley.request.MultiPartRequest;
+import com.android.volley.request.SimpleMultiPartRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private ImageView imageView;
+    private ProgressBar progressBar;
+
+    //public static String BASE_URL = "https://raavi.news/file_upload/upload.php";
+    public static String BASE_URL = "http://192.168.1.3:8888/file_uploading/upload.php";
+    static final int PICK_IMAGE_REQUEST = 1;
+    String filePath;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.theImage);
+        Button btnChoose = findViewById(R.id.btnChoose);
+        Button btnUpload = findViewById(R.id.btnUpload);
+
+        btnChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageBrowse();
+            }
+        });
+
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (filePath != null)
+                    imageUpload(filePath);
+                else
+                    Toast.makeText(MainActivity.this, "File Not Selected.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private void imageBrowse() {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Video.Media.EXTERNAL_CONTENT_URI);
+        // Start the Intent
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+
+            if(requestCode == PICK_IMAGE_REQUEST){
+                Uri picUri = data.getData();
+
+                filePath = getPath(picUri);
+
+                //imageView.setImageURI(picUri);
+
+            }
+
+        }
+
+    }
+
+    private void imageUpload(final String imagePath) {
+
+        SimpleMultiPartRequest smr = new SimpleMultiPartRequest(Request.Method.POST, BASE_URL,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("Response", response);
+                        try {
+                            JSONObject jObj = new JSONObject(response);
+                            String message = jObj.getString("message");
+
+                            Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+
+                        } catch (JSONException e) {
+                            // JSON error
+                            e.printStackTrace();
+                            Toast.makeText(getApplicationContext(), "Json error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(), "MSG: "+error.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+
+        smr.addFile("file", imagePath);
+        Log.d(TAG, "imageUpload: +"+imagePath);
+        MyApplication.getInstance().addToRequestQueue(smr);
+
+    }
+
+    private String getPath(Uri contentUri) {
+        String[] proj = { MediaStore.Images.Media.DATA };
+        CursorLoader loader = new CursorLoader(getApplicationContext(), contentUri, proj, null, null, null);
+        Cursor cursor = loader.loadInBackground();
+        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(column_index);
+        cursor.close();
+        return result;
+    }
+
+}
